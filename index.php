e41cdc97f0802ed4fc76c49c1ba0dbbc2f4df0f2
patrik <patrik@silentridge>
2021-08-04 00:08:22 +0200

    asd

--- index.php
+++ index.php
@@ -6,8 +6,9 @@ foreach($files as $file){
 	echo $file."-".$dir;die();
 	$data .= file_get_contents($dir."/".$file);
 	$index = file_get_contents("base.html");
-	$index_new = str_replace("{{BODY}}", $data, $index);
-	file_put_contents(__DIR__."index.html", $index_new);
+	$index_new = str_ireplace("{{BODY}}", $index, $data);
+	$ret =	file_put_contents(__DIR__."/index.html", $index_new);
+	echo $ret;
 }
 
 
